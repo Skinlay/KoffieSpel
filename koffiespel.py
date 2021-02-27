74276b9c8d8f4744a85b8587226183014405da29
Tim Stoop <tim@kumina.nl>
2021-02-27 14:34:59 +0100

    Get simple flow to work, including tests

--- koffiespel.py
+++ koffiespel.py
@@ -5,25 +5,29 @@ import sys
 class KoffieSpel:
     def __init__(self):
         self.getal = random.randint(1, 100)
-        self.guess = -1
 
     def play(self):
-        self.ask()
+        self.ask(aantal=0)
 
-    def ask(self, out=sys.stdout):
-        out.write("I have a number in mind between 1 and 100, guess which it is: ")
-        self.guess = int(input())
-        self.check(out=out)
+    def ask(self, aantal, out=sys.stdout):
+        if aantal == 0:
+            out.write("I have a number in mind between 1 and 100, guess which it is: ")
+        else:
+            out.write("Try again! ")
+        poging = int(input())
+        self.check(poging=poging, out=out)
 
-    def check(self, out=sys.stdout):
-        while self.guess != self.getal:
-            if self.guess > self.getal:
-                out.write("to high")
-            elif self.guess < self.getal:
-                out.write("to low")
-            self.guess = int(input())
-        out.write("correct")
-        exit()
+    def check(self, poging, out=sys.stdout):
+        if poging > self.getal:
+            out.write("Too high! ")
+        elif poging < self.getal:
+            out.write("Too low!")
+        elif poging == self.getal:
+            out.write('Correct!')
+            sys.exit(0)
+        else:
+            out.write('How did you get in this part of the loop? You hacker!')
+            sys.exit(1)
 
 
 if __name__ == "__main__":
--- tests/test_getallen_raden.py
+++ tests/test_getallen_raden.py
@@ -28,16 +28,21 @@ class TestKoffiespel(unittest.TestCase):
             verschil = True
         self.assertTrue(verschil)
 
-    @patch('builtins.input', return_value = '10')
+    @patch('builtins.input', lambda *args: '10')
     def test_vraag_om_getal(self):
         out = StringIO()
-        self.koffie.ask(out=out)
-        self.assertEqual(out.getvalue(), "I have a number in mind between 1 and 100, guess which it is: ")
+        self.koffie.ask(out=out, aantal=0)
+        self.assertIn(out.getvalue(), ["I have a number in mind between 1 and 100, guess which it is: Too low!",
+            "I have a number in mind between 1 and 100, guess which it is: Too high!"])
 
     @patch('builtins.input', lambda *args: '10')
     def test_kijk_of_getal_gelijk_is(self):
         out = StringIO()
         self.koffie.getal = 10
-        self.koffie.ask(out=out)
-        print(out.getvalue())
-        self.assertEqual(0, 0)
+        with self.assertRaises(SystemExit) as err:
+            self.koffie.ask(out=out, aantal=0)
+        self.assertEqual(out.getvalue(), "I have a number in mind between 1 and 100, guess which it is: Correct!")
+
+
+if __name__ == '__main__':
+    unittest.main(exit=False)
